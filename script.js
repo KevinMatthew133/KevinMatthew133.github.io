5db220fd5ed92221db8c4ce489881339928ab9c5
Kevin Matthew <118345180+KevinMatthew133@users.noreply.github.com>
2024-03-22 17:32:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function changeImgBocchi(){
+    document.getElementById('chars').src='../images/Bocchi.png';
+    document.getElementById('desc').innerHTML = "Role: Lead Guitarist, Lyricist <br> Seiyu: Yoshino Aoyama <br> <br> An extremely timid and introverted first-year student in high school. She is in charge of the lead guitar of Kessoku Band. She dreams of playing in a band that might let even an introverted girl shine and starts playing the guitar. Although her skill is the real deal, she can't show it well in a band or public. She has a habit of saying \"Ahh\" before she says something."
+}
+function changeImgNijika(){
+    document.getElementById('chars').src='../images/Nijika.png';
+    document.getElementById('desc').innerHTML = "Role: Drummer, Organizer <br> Seiyu: Sayumi Suzushiro <br> <br> An energetic and cheerful second-year high school student. She is the drummer of Kessoku Band. The organizer of her band who takes care of Hitori was already joined Kessoku Band. She has an older sister, Seika, who is the manager of the live house \"STARRY\" and has a special feeling for the live house.";
+}
+function changeImgKita(){
+    document.getElementById('chars').src='../images/Kita.png';
+    document.getElementById('desc').innerHTML = "Role: Vocalist, Guitarist <br> Seiyu: Ikumi Hasegawa <br> <br> A bright and popular first year of high school. She is in charge of the guitar and vocals of Kessoku Band. She is a cheerful character who likes to interact with people and can talk to her with a smile without hesitation, even when she meets her for the first time. She looks up to Ryo, and her feelings can go too far. She often takes photos and uploads to Isosta."
+}
+function changeImgRyo(){
+    document.getElementById('chars').src='../images/Ryo.png';
+    document.getElementById('desc').innerHTML = "Role: Bassist, Backing Vocalist <br> Seiyu: Saku Mizuno <br> <br> A cool and solitary second-year high school student. She is the bassist of Kessoku Band and Nijika's best friend. Her hobbies are out of the ordinary, and she rejoices when called a weirdo. She lives with a wealthy family but never has any money because she spends it all on instruments. She occasionally eats weeds to get rid of her hunger."
+}
+
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+  
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
